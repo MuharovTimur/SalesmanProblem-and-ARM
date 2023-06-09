655e0de6d5ef4ca436b86e64e5645bfaed2e608c
Muharov Timur(ris-22-1b) <116500342+MuharovTimur@users.noreply.github.com>
2023-06-09 19:53:34 +0500

    Add files via upload

--- Part 1 ARM/mainwindow.cpp
+++ Part 1 ARM/mainwindow.cpp
@@ -1,18 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include"neworder.h"
-#include<QDate>
-#include<QDateTime>
-#include<QTextCharFormat>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->tableWidget->setRowCount(5);
-    ui->tableWidget->setColumnCount(3);
-    ui->tableWidget->setHorizontalHeaderLabels((QStringList()<<"Заказчик"<<"Наименование"<<"Стоимость"));
+    ui->tableWidget->setRowCount(10);
+    ui->tableWidget->setColumnCount(4);
+    ui->tableWidget->setHorizontalHeaderLabels((QStringList()<<"Заказчик"<<"Наименование"<<"Стоимость"<<"Выполнение заказа"));
 }
 
 MainWindow::~MainWindow()
@@ -40,4 +36,28 @@ void MainWindow::on_pushButton_clicked()
         ui->tableWidget->setItem(rownum,2,tbl2);
     rownum++;
 }
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString orderer=ui->lineEdit->text(),name=ui->lineEdit_2->text(),price=ui->lineEdit_3->text(),done="Выполнено";
+    QDate dt=ui->dateEdit->date();
+    QTextCharFormat format;
+    QBrush brush;
+    QColor color;
+    int r=0,g=0,b=0,a=120;
+    color.setRgb(r,g,b,a);
+    brush.setColor(color);
+    format.setBackground(brush);
+    ui->calendarWidget->setDateTextFormat(dt,format);
+        QTableWidgetItem *tbl = new QTableWidgetItem(orderer);
+        ui->tableWidget->setItem(rownum,0,tbl);
+        QTableWidgetItem *tbl1 = new QTableWidgetItem(name);
+        ui->tableWidget->setItem(rownum,1,tbl1);
+        QTableWidgetItem *tbl2 = new QTableWidgetItem(price);
+        ui->tableWidget->setItem(rownum,2,tbl2);
+        QTableWidgetItem *tbl3 = new QTableWidgetItem(done);
+        ui->tableWidget->setItem(rownum,3,tbl3);
+    rownum++;
+}
+
 
